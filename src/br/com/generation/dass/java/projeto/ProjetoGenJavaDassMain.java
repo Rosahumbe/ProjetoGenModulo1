b42d1c3973345441e62ed235ad549e9b530c7d01
annieSan <annie.santtana@gmail.com>
2022-06-10 17:45:21 -0300

    atualizações annie

--- src/br/com/generation/dass/java/projeto/ProjetoGenJavaDassMain.java
+++ src/br/com/generation/dass/java/projeto/ProjetoGenJavaDassMain.java
@@ -10,22 +10,9 @@ public class ProjetoGenJavaDassMain {
 		String nome;
 		int idade;
 		
-		System.out.println("====================================================================");
-		System.out.println("|                                                                  |");
-		System.out.println("|   Seja bem-vindo ao Java Dass21                   (ɔ◔‿◔)ɔ ♥      |");
-		System.out.println("|                                                                  |");
-		System.out.println("|   Este protótipo foi criado com o propósito de auxiliar pessoas  |");
-		System.out.println("|   a identificar sintomas relacionados a depressão, ansiedade e   |");
-		System.out.println("|   estress e foi baseado no no DASS21 (Depression, Anxiety and    |");
-		System.out.println("|   and Stress Scale)                                              |");
-		System.out.println("|                                                                  |");
-		System.out.println("|                    ::::::::: ATENÇÃO :::::::::                   |"); 
-		System.out.println("|                                                                  |");
-		System.out.println("|   O resultado da avaliação não indica um diagnóstico conclusivo. |");
-		System.out.println("|  Para determinar qualquer diagnóstico potencial discuta seu      |");
-		System.out.println("|  resultado com um psicólogo ou um médico psiquiatra.             |");
-		System.out.println("====================================================================");
-		
+	Interface telaInicial = new Interface();
+	telaInicial.saudacao();
+	
 		//dados do "paciente"
 		System.out.println("Por favor, informe seu nome");
 		nome = leia.nextLine();
