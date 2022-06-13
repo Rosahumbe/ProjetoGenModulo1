e9db2279ef819db4e84293047e09d10cb54b81ee
Beatriz <beatriizramoss@gmail.com>
2022-06-13 14:40:26 -0300

    adicionado colaboradores

--- src/br/com/generation/dass/java/perguntas/PerguntasEstress.java
+++ src/br/com/generation/dass/java/perguntas/PerguntasEstress.java
@@ -1,5 +1,17 @@
 package br.com.generation.dass.java.perguntas;
 
+import java.util.Scanner;
+
 public class PerguntasEstress {
+	
+	Scanner leia = new Scanner(System.in);
+
+	public int questao1() {
+		PerguntasUtil pergunta1 = new PerguntasUtil();
+		return pergunta1.esqueletoPerguntas("Achei difícil me acalmar ", " 1- Não se aplicou de maneira alguma \n "
+				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
+				+ "3- Aplicou-se em um grau considerÃ¡vel, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
+	
+	}
 
 }
