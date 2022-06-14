99d8660f546489f333944cc3fd6b66837d2a35a3
DevDaniel47 <daniel.oliveira0622@gmail.com>
2022-06-14 15:02:29 -0300

    Padronização de conteúdo nas classes perguntas, interface e resultado

--- src/br/com/generation/dass/java/perguntas/PerguntasAnsiedade.java
+++ src/br/com/generation/dass/java/perguntas/PerguntasAnsiedade.java
@@ -11,49 +11,49 @@ public class PerguntasAnsiedade {
 
 	public int questao1() {
 		PerguntasUtil pergunta1 = new PerguntasUtil();
-		return pergunta1.esqueletoPerguntas("Senti minha boca seca: ", " 1- Não se aplicou de maneira alguma \n "
+		return pergunta1.esqueletoPerguntas("1. Senti minha boca seca: ", " 1- Não se aplicou de maneira alguma \n "
 				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 				+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 		
 	}
 	public int questao2() {
 		PerguntasUtil pergunta2 = new PerguntasUtil();
-		return pergunta2.esqueletoPerguntas("Tive dificuldade em respirar em alguns momentos (ex. respiração ofegante, falta de ar, sem ter feito nenhum esforço físico)", " 1- Não se aplicou de maneira alguma \n "
+		return pergunta2.esqueletoPerguntas("2. Tive dificuldade em respirar em alguns momentos (ex. respiração \n   ofegante, falta de ar, sem ter feito nenhum esforço físico): ", " 1- Não se aplicou de maneira alguma \n "
 				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 				+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 		
 	}
 	public int questao3() {
 		PerguntasUtil pergunta3 = new PerguntasUtil();
-		return pergunta3.esqueletoPerguntas("Senti tremores (ex. nas mãos)", " 1- Não se aplicou de maneira alguma \n "
+		return pergunta3.esqueletoPerguntas("3. Senti tremores (ex. nas mãos): ", " 1- Não se aplicou de maneira alguma \n "
 				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 				+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 		
 	}
 	public int questao4() {
 		PerguntasUtil pergunta4 = new PerguntasUtil();
-		return pergunta4.esqueletoPerguntas("Preocupei-me com situações em que eu pudesse entrar em pânico e parecesse ridículo(a)", " 1- Não se aplicou de maneira alguma \n "
+		return pergunta4.esqueletoPerguntas("4. Preocupei-me com situações em que eu pudesse entrar em pânico e \n   parecesse ridículo(a): ", " 1- Não se aplicou de maneira alguma \n "
 				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 				+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 		
 	}
 	public int questao5() {
 		PerguntasUtil pergunta5 = new PerguntasUtil();
-		return pergunta5.esqueletoPerguntas("Senti que ia entrar em pânico", " 1- Não se aplicou de maneira alguma \n "
+		return pergunta5.esqueletoPerguntas("5. Senti que ia entrar em pânico: ", " 1- Não se aplicou de maneira alguma \n "
 				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 				+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 		
 	}
 	public int questao6() {
 		PerguntasUtil pergunta6 = new PerguntasUtil();
-		return pergunta6.esqueletoPerguntas("Sabia que meu coração estava alterado mesmo não tendo feito nenhum esforço físico (ex. aumento da frequência cardíaca, disritmia cardíaca)", " 1- Não se aplicou de maneira alguma \n "
+		return pergunta6.esqueletoPerguntas("6. Sabia que meu coração estava alterado mesmo não tendo feito nenhum \n   esforço físico (Ex. aumento da frequência cardíaca, disritmia \n   cardíaca): ", " 1- Não se aplicou de maneira alguma \n "
 				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 				+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 		
 	}
 	public int questao7() {
 		PerguntasUtil pergunta7 = new PerguntasUtil();
-		return pergunta7.esqueletoPerguntas("Senti medo sem motivo", " 1- Não se aplicou de maneira alguma \n "
+		return pergunta7.esqueletoPerguntas("7. Senti medo sem motivo: ", " 1- Não se aplicou de maneira alguma \n "
 				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 				+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 		
--- src/br/com/generation/dass/java/perguntas/PerguntasEstresse.java
+++ src/br/com/generation/dass/java/perguntas/PerguntasEstresse.java
@@ -9,49 +9,49 @@ public class PerguntasEstresse {
 
 	public int questao15() {
 		PerguntasUtil pergunta15 = new PerguntasUtil();
-		return pergunta15.esqueletoPerguntas("Achei difícil me acalmar ", " 1- Não se aplicou de maneira alguma \n "
+		return pergunta15.esqueletoPerguntas("15. Achei difícil me acalmar: ", " 1- Não se aplicou de maneira alguma \n "
 				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 				+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 	}
 	
 	public int questao16() {
 	PerguntasUtil pergunta16 = new PerguntasUtil();
-	return pergunta16.esqueletoPerguntas("Tive a tend�ncia de reagir de forma exagerada �s situações ", " 1- Não se aplicou de maneira alguma \n "
+	return pergunta16.esqueletoPerguntas("16. Tive a tend�ncia de reagir de forma exagerada ás situações: ", " 1- Não se aplicou de maneira alguma \n "
 				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 				+ "3- Aplicou-se em um grau consider�vel, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 	}
 
 	public int questao17 () {
 	PerguntasUtil pergunta17 = new PerguntasUtil();
-	return pergunta17.esqueletoPerguntas("Senti que estava sempre nervoso ", " 1- Não se aplicou de maneira alguma \n "
+	return pergunta17.esqueletoPerguntas("17. Senti que estava sempre nervoso: ", " 1- Não se aplicou de maneira alguma \n "
 				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 				+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 	}
 	
 	public int questao18() {
 	PerguntasUtil pergunta18 = new PerguntasUtil();
-	return pergunta18.esqueletoPerguntas("Senti-me agitado ", " 1- N�o se aplicou de maneira alguma \n "
+	return pergunta18.esqueletoPerguntas("18. Senti-me agitado: ", " 1- Não se aplicou de maneira alguma \n "
 				+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 				+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 	}
 	
 	public int questao19() {
 		PerguntasUtil pergunta19 = new PerguntasUtil();
-		return pergunta19.esqueletoPerguntas("Achei difícil relaxar ", " 1- N�o se aplicou de maneira alguma \n "
+		return pergunta19.esqueletoPerguntas("19. Achei difícil relaxar: ", " 1- Não se aplicou de maneira alguma \n "
 					+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 					+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 	}
 	
 	public int questao20() {
 		PerguntasUtil pergunta20 = new PerguntasUtil();
-		return pergunta20.esqueletoPerguntas("Fui intolerante com as coisas que me impediam de continuar o que eu estava fazendo ", " 1- Não se aplicou de maneira alguma \n "
+		return pergunta20.esqueletoPerguntas("20. Fui intolerante com as coisas que me impediam de continuar o que \n   eu estava fazendo: ", " 1- Não se aplicou de maneira alguma \n "
 					+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 					+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 	}
 	
 	public int questao21() {
 		PerguntasUtil pergunta21 = new PerguntasUtil();
-		return pergunta21.esqueletoPerguntas("Senti que estava um pouco emotivo/sensível demais ", " 1- Não se aplicou de maneira alguma \n "
+		return pergunta21.esqueletoPerguntas("21. Senti que estava um pouco emotivo/sensível demais: ", " 1- Não se aplicou de maneira alguma \n "
 					+ "2- Aplicou-se em algum grau, ou por pouco de tempo \n "
 					+ "3- Aplicou-se em um grau considerável, ou por uma boa parte do tempo \n 4- Aplicou-se muito, ou na maioria do tempo");
 	}
